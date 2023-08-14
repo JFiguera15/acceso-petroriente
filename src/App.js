5b81340b5de408ff99f56f140285f2b70d9a0214
JFiguera15 <juanantoniofiguera@gmail.com>
2023-08-14 15:16:21 -0400

    An attempt

--- src/App.js
+++ src/App.js
@@ -121,7 +121,7 @@ export default function App() {
       <div>
         <BrowserRouter>
           <Routes>
-            <Route path="/zonaindustrial" element={<zonaindustrial />} />
+            <Route path="/zonaindustrial" element={< zonaindustrial />} />
           </Routes>
         </BrowserRouter>
       </div>
