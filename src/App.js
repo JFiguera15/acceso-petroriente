0efafaf02d1712440e4a8993a22aa3fc961257a3
JFiguera15 <juanantoniofiguera@gmail.com>
2023-08-15 11:09:21 -0400

    Finally?

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import Petroriente from "./pages/Petroriente";
+import ZonaIndustrial from "./pages/zonaindustrial";
 
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
         <BrowserRouter>
           <Routes>
             <Route index element = { <Petroriente /> } />
+            <Route path="/zonaindustrial" element = { <ZonaIndustrial />}></Route>
             <Route path="/petroriente" element={<Petroriente />} />
           </Routes>
         </BrowserRouter>
